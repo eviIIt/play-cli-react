8d3ebaaeaf473532f0348339f165df56cc1e3f20
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-13 07:44:21 +0000

    Deploy eviIIt/play-cli-react to github.com/eviIIt/play-cli-react.git:gh-pages

---
+++ precache-manifest.0be3178c9b5b8df29c8b4c5f89da3002.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e325f758137c6b5f24cea334da5095",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9b33ea9cb77698346c0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e74f686c7311110ec462",
+    "url": "/static/js/2.98360460.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.98360460.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9b33ea9cb77698346c0",
+    "url": "/static/js/main.2f7b0efe.chunk.js"
+  },
+  {
+    "revision": "0a1a0029efec80e99377",
+    "url": "/static/js/runtime-main.55498eba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
